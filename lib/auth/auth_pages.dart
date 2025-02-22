76d405171f771320271755843525a8f73b46de00
UxairXaiser <uzairqaiser14@gmail.com>
2025-02-22 15:05:14 +0500

    Add comment to LoginPage class for clarity

--- lib/auth/auth_pages.dart
+++ lib/auth/auth_pages.dart
@@ -3,6 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 class LoginPage extends StatefulWidget {
+  // This page handles user login
   const LoginPage({super.key});
 
   @override
